e41ada38bf46ff4ad59fa3d33c3d650255c3a200
ZohaibZiri <99802150+ZohaibZiri@users.noreply.github.com>
2022-03-16 14:40:41 +0000

    Add files via upload
    version 1

---
+++ Front end v1/script/script.js
@@ -0,0 +1,15 @@
+function para_arrow()
+{
+	
+	document.getElementById("arrow").innerHTML = "Hello this topic is based on Internet of things"; 
+	
+}
+
+function para_third_year()
+{
+	
+	document.getElementById("third_year").innerHTML = "Hello i am a third year student finishing my last year of studies"; 
+	
+}         
+
+  
